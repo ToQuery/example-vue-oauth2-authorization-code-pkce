d11b21d71b5e13695bba66f5a07ed45963f27ba1
ToQuery <toquery@qq.com>
2024-02-04 01:30:50 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {fileURLToPath} from "node:url";
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    server: {
+        host: true,
+    },
+    plugins: [
+        vue(),
+        vueJsx(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
